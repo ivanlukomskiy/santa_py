b890521a1d7bdd5bdac4a4f3e9f624b5776f73f3
ivanlukomskiy <ivanlukomskiy@yandex-team.ru>
2019-12-16 16:13:28 +0300

    Groups support

--- santa.py
+++ santa.py
@@ -1,3 +1,4 @@
+import itertools
 import time
 
 import yaml
@@ -6,6 +7,7 @@ import random
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from sortedcontainers import SortedDict
 
 SUBJECT = 'Тайный Санта'
 
@@ -43,8 +45,53 @@ def send_multiple(participants):
     session.quit()
 
 
+def order(participants):
+    def count_score(order):
+        score = 0
+        for i, participant in enumerate(order):
+            next = order[i + 1 if i + 1 < len(order) else 0]
+            if participant['sex'] and next['sex'] and participant['sex'] != next['sex']:
+                score += len(order)
+            if participant['group'] and next['group'] and participant['group'] == next['group']:
+                score -= len(order) ** 2 + 1
+        return score
+
+    def offer_order():
+        copy = participants.copy()
+        random.shuffle(copy)
+        return copy
+
+    scores_map = SortedDict()
+
+    best_order = participants
+    best_score = 0
+
+    for _ in itertools.repeat(None, 100000):
+        order = offer_order()
+        score = count_score(order)
+
+        scores_map[score] = scores_map.get(score, 0) + 1
+
+        # names = [p['name'] for p in order]
+        # print(score, names)
+
+        if score > best_score:
+            best_order = order
+            best_score = score
+
+    print('Scores distribution')
+    for k in scores_map.keys():
+        v = scores_map.get(k)
+        print(f'{k} -> {v}')
+
+    return best_order
+
+
 config = yaml.safe_load(open("smtp.yaml", "r"))['mail']['smtp']
 template = open("template.html", "r", encoding="utf-8").read()
 participants = get_participants()
-random.shuffle(participants)
+participants = order(participants)
+for participant in participants:
+    print(participant)
+
 send_multiple(participants)
